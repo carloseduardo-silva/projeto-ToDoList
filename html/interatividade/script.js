7d8fa8c7158100aaf198704304b28ec99e6f02c8
Carlos Eduardo <102683309+carloseduardo-silva@users.noreply.github.com>
2022-08-30 12:04:18 -0300

    html + css  pronto, JS 20%

--- html/interatividade/script.js
+++ html/interatividade/script.js
@@ -1,7 +1,70 @@
+const tarefareal = document.getElementById('tarefa')
+const todoList = document.querySelector('div#resp')
+
 function adicionar() {
-    var tarefareal = document.getElementById('tarefa')
-    localStorage.setItem('tarefa', tarefareal.value)
+
+    const tarefalrealvalue = tarefareal.value
+
+    if (tarefalrealvalue) {
+        saveInputTarefa(tarefalrealvalue)
+    }
+
+}
+
+const saveInputTarefa = (text) => {
+
+    const todo = document.createElement('div');
+    todo.classList.add('todo');
+
+    const todoTitle = document.createElement('h3');
+    todoTitle.innerText = text;
+    todo.appendChild(todoTitle);
+    
+    const doneBtn = document.createElement('button');
+    doneBtn.classList.add('finish-todo');
+    doneBtn.innerHTML = "✔";
+    todo.appendChild(doneBtn)
+    
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('finish-todo');
+    editBtn.innerHTML = "✎";
+    todo.appendChild(editBtn)
+    
+    const excludeBtn = document.createElement('button');
+    excludeBtn.classList.add('finish-todo');
+    excludeBtn.innerHTML = "✘";
+    todo.appendChild(excludeBtn)
+    
+    todoList.appendChild(todo)
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*  Meu jeito, porem nao tao funcional:   localStorage.setItem('tarefa', tarefareal.value)
     
     var resposta = document.getElementById('resp')
-    resposta.innerHTML += `<p> ${localStorage.getItem('tarefa')}  </p> `
-}
+    resposta.innerHTML += `<p> ${localStorage.getItem('tarefa')}  
+    <input class="check" type="button" value="✔" onclick="check()"> 
+    <a id = "editar" href="index002.html"> ✎ </a>  
+    <input class="excluir" type="button" value="✘"> 
+    
+    </p> ` */
